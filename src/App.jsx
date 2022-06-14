e188657fb24cd419b92e30636909c50d83059649
programmer-tm <65005139+programmer-tm@users.noreply.github.com>
2022-06-14 22:22:50 +0300

    DZ_3
    DZ_3 без разделения по экрану формы и чата. но + проверки на пустоту.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,40 @@
 import React, {useState, useEffect, useRef} from "react";
 import './App.css';
 import Message from './Message';
+import { Box,TextField, Button } from "@mui/material";
 var id = 1;
 
 function App() {
   const [messageList, setMessageList] = useState([]);
   var autor = useRef('');
-  var text = useRef('');
   
   useEffect(()=>{
-    setTimeout(()=>{
-    console.log(autor.current.value+", you message sended");},1000)
+    if (document.getElementById('autor').value){
+      setTimeout(()=>{
+        console.log(document.getElementById('autor').value+", you message sended");},1000)
+    }
+    
   },[autor.current.value, messageList]);
 
   function send(){
-    setMessageList(messageList.concat([{id: id, autor: autor.current.value, text: text.current.value}]));
+    setMessageList(messageList.concat([{id: id, autor: document.getElementById('autor').value, text: document.getElementById('text').value}]));
     id++;
   }
   return (
-    <div className="App">
-      <form action="#">
-      <input type="text" ref={autor}/>
-      <input type="text" ref={text}/>
-      <button onClick={send}>Send</button>
-      </form>
+
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '20ch', height: '7.5ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div><TextField id="autor" label="Автор" variant="outlined" autoFocus /></div>
+      <div><TextField id="text" label="Текст" variant="outlined" autoFocus /></div>
+      <Button onClick={send} variant="outlined">Отправить</Button>
       <Message messageList={messageList}/>
-    </div>
+    </Box>
   );
 }
 
--- src/Message.jsx
+++ src/Message.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Typography from '@mui/material/Typography';
 
-function Message({messageList}) {
-    return (
-    <ol>
+export default function Message({messageList}) {
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {messageList.map((message) => (
-        <li key={message.id}>Автор:{message.autor} Сообщение: {message.text}</li>
+      <ListItem alignItems="flex-start" key={message.id}>
+        <ListItemText
+          primary="Новое сообщение:"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: 'inline' }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {!!(message.autor)?message.autor:"Anonimus"}
+              </Typography>
+              {": "}{!!(message.text)?message.text:"No message!"}
+            </React.Fragment>
+          }
+        />
+      </ListItem>
       ))}
-    </ol>
-    );
-  }
-  
-  export default Message;
+    </List>
+  );
+}
